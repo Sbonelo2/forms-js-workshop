761cb9f93d44aa1db240cf55a16c6fc96d279c4a
Sbonelo2 <cbonelo224@gmail.com>
2025-08-19 08:56:16 +0200

    windows

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import NumberWork from "./numberWork.js";
-
+windows.start = () =>{
 const num1 = new NumberWork(24);
 num1.displayNumber();
 num1.pozOrNeg();
+
+
+
+
+
+
+
+}
+windows.start();
+
--- FormControl.js
+++ FormControl.js
@@ -1,9 +1,9 @@
-import NumberWork from "./numberWork.js"
-
-class formControl1{
-    constructor2 (num1)
+export default class FormControl{
+constructor(num1){
     this.num1 = num1
-    submithandler(){
-        success: x
+    }
+    submitHandler(){
+       const success = Number(this.num1);
+       alert('success', success);
     }
 }
